e11947d8367256507963069ce2b3541ed352e83d
XxLegitOPxX <78969143+XxLegitOPxX@users.noreply.github.com>
2024-08-03 03:17:22 +0100

    port scripts to 1.20.1

--- forge/client/kubejs/client_scripts/script.js
+++ forge/client/kubejs/client_scripts/script.js
@@ -1,21 +1,19 @@
 // priority: 0
 
-var recipeBlacklist = [
+const recipeBlacklist = [
   "cfm:oak_mail_box",
   "cfm:post_box",
-  "minecraft:ender_chest",
   "chunkloaders:advanced_chunk_loader",
   "chunkloaders:ultimate_chunk_loader",
 ];
-var conflictingRecipes = ["additionaladditions:wrench"];
+const conflictingRecipes = ["additionaladditions:wrench"];
 
-onEvent("item.tooltip", (tooltip) => {
-  // Add tooltip to all of these items
-  tooltip.add(
+ItemEvents.tooltip((event) => {
+  event.add(
     recipeBlacklist,
     Text.red("This item is prohibited on the server.")
   );
-  tooltip.add(
+  event.add(
     conflictingRecipes,
     Text.yellow("This item has a conflicting recipe.")
   );
