d630f9d6c1b38c60c4ace43f3f9805593fe4c808
XxLegitOPxX <XxLegitOPxX@users.noreply.github.com>
2024-08-03 06:29:35 +0000

    deploy: 8223271571e0cc1cd1d500b93eef3d1016249521

--- forge/client/kubejs/startup_scripts/script.js
+++ forge/client/kubejs/startup_scripts/script.js
@@ -1,22 +1,13 @@
 // priority: 0
 
-console.info('Hello, World! (You will only see this line once in console, during startup)')
-
-onEvent('item.registry', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
-
-	// Yamato
-	event.create('yamato', 'sword')
-		.displayName("Yamato")
-		//.texture("kubejs:item/yamato")
-		.tooltip(Text.aqua("Now I'm motivated!"))
-		.tier('diamond')
-		//.attackDamageBaseline(4)
-		.speedBaseline(0)
-})
-
-onEvent('block.registry', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
+StartupEvents.registry("item", (event) => {
+  // Yamato
+  event
+    .create("yamato", "sword")
+    .displayName("Yamato")
+    //.texture("kubejs:item/yamato")
+    .tooltip(Text.aqua("Now I'm motivated!"))
+    .tier("diamond")
+    //.attackDamageBaseline(4)
+    .speedBaseline(0);
+});
